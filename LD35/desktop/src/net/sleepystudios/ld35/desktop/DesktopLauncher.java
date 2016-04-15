9217509cd9e0063e9897a8ca55992439f04661f2
Tudor Sandulescu <sekaru@sleepystudios.com>
2016-04-15 21:26:15 +0100

    Initial commit

---
+++ LD35/desktop/src/net/sleepystudios/ld35/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.sleepystudios.ld35.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.sleepystudios.ld35.LD35;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LD35(), config);
+	}
+}
